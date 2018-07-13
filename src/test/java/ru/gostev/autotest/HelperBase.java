8d62808cb60a3343fd535a4a2c35c49942d6d73b
VladimirGostev <vladi-gos@yandex.ru>
2018-07-13 09:09:30 +0300

    Добавил базовый класс HelperBase для класса помошника YandexMainPageHelper

---
+++ src/test/java/ru/gostev/autotest/HelperBase.java
@@ -0,0 +1,27 @@
+package ru.gostev.autotest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HelperBase {
+  protected WebDriver wd;
+
+  public HelperBase(WebDriver wd) {
+    this.wd = wd;
+  }
+
+  public void type(By locator, String text){
+    click(locator);
+    if(text != null){
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if(! text.equals(existingText)){
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
+  }
+
+  public void click(By locator){
+    wd.findElement(locator).click();
+  }
+}
